3eb274c89b7c5d56e369027967c53d42ac84560a
Md Asif Alam <54733354+measifalam@users.noreply.github.com>
2022-04-11 17:53:06 +0530

    Time: 30 ms (67.11%), Space: 25.2 MB (89.59%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cs
+++ 7-reverse-integer/7-reverse-integer.cs
@@ -1,30 +1,31 @@
 public class Solution {
-    public int Reverse(int x) {
+    public int Reverse(int x) 
+    {
         bool flag = true;
-if(x<0)
-{
-    flag=false; x=-x;
-}
+        if(x<0)
+        {
+           flag=false; x=-x;
+        }
 
 
-long y=0;
-while(x>0)
-{
- int temp=x%10; 
- y=y*10+temp;
- x=x/10;
- }
+        long y=0;
+        while(x>0)
+        {
+           int temp=x%10; 
+           y=y*10+temp;
+           x=x/10;
+        }
  
- if(flag==false)
- {
- y=-y;
- }
+       if(flag==false)
+        {
+           y=-y;
+        }
  
- if(y<int.MinValue || y>int.MaxValue)
- {
- return 0;
- 
- }
- return (int)y;
+      if(y<int.MinValue || y>int.MaxValue)
+        {
+           return 0;
+        }
+      
+     return (int)y;
     }
 }
