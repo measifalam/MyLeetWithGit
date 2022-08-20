f3e506ae905c968d9ccb7e333585a4399f2f5d5a
Md Asif Alam <54733354+measifalam@users.noreply.github.com>
2022-08-20 17:19:48 +0530

    Time: 345 ms (8.87%), Space: 83.5 MB (33.96%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let container = '';
+    while(head!==null)
+        {
+            container = container + head.val;
+            head = head.next
+        }
+    return container === container.split('').reverse().join('')
+    
+};
